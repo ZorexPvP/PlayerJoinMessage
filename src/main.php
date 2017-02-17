645e8fbb6f6f46627821d4af4c851adf7ca22dcf
ZorexPvP <zorex.pvp@gmail.com>
2017-02-16 21:56:12 -0600

    coded main

--- src/main.php
+++ src/main.php
@@ -1,1 +1,19 @@
-.
+<?php
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\PlayerJoinEvent;
+
+class main extends PluginBase implements Listener{
+  
+  public function onPlayerJoinEvent(PlayerJoinEvent $event){
+    
+    $player = $event->getPlayer;
+    $name = $player->getPlayerName;
+    $player->sendMessage(TextFormat::BLUE . "Welcome" . TextFormat::WHITE . $name . TextFormat::BLUE . " To This Server!");
+    
+  }
+}
+  
